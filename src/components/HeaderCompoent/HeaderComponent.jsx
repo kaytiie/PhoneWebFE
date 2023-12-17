2029ee4fd85242e103312c0fde4398f11471c304
Tran Dinh Thi <gigidream9pro@gmail.com>
2023-12-17 09:56:53 +0700

    Gio hang

--- src/components/HeaderCompoent/HeaderComponent.jsx
+++ src/components/HeaderCompoent/HeaderComponent.jsx
@@ -157,20 +157,51 @@ const HeaderComponent = ({ isHiddenSearch = false, isHiddenCart = false }) => {
               )}
             </WrapperHeaderAccout>
           </Loading>
-          {!isHiddenCart && (
-            <div onClick={() => navigate('/order')} style={{ cursor: 'pointer' }}>
-              <Badge count={order?.orderItems?.length} size="small">
-                <ShoppingCartOutlined style={{ fontSize: '30px', color: '#fff' }} />
-              </Badge>
-              <WrapperTextHeaderSmall>Giỏ hàng</WrapperTextHeaderSmall>
-            </div>
+          {user?.access_token ? (
+            <span>
+              {!isHiddenCart && (
+                <div onClick={() => navigate('/order')} style={{ cursor: 'pointer' }}>
+                  <Badge count={order?.orderItems?.length} size="small">
+                    <ShoppingCartOutlined style={{ fontSize: '30px', color: '#fff' }} />
+                  </Badge>
+                  <WrapperTextHeaderSmall>Giỏ hàng</WrapperTextHeaderSmall>
+                </div>
+              )}
 
-          )}
+            </span>
+          ) : (
+            <span>
+              {!isHiddenCart && (
+                <div onClick={() => navigate('/sign-in')} style={{ cursor: 'pointer' }}>
+                  <Badge count={order?.orderItems?.length} size="small">
+                    <ShoppingCartOutlined style={{ fontSize: '30px', color: '#fff' }} />
+                  </Badge>
+                  <WrapperTextHeaderSmall>Giỏ hàng</WrapperTextHeaderSmall>
+                </div>
+              )}
+
+            </span>
+          )
+          }
           <span>
-            <div onClick={() => navigate('/chatpage')} style={{ cursor: 'pointer' }}>
-              <WechatOutlined style={{ fontSize: '30px', color: '#fff' }} />
-              <WrapperTextHeaderSmall>Thảo luận</WrapperTextHeaderSmall>
-            </div>
+            {
+              user?.isAdmin ? (
+                <div onClick={() => navigate('/chatpageadmin')} style={{ cursor: 'pointer' }}>
+                  <WechatOutlined style={{ fontSize: '30px', color: '#fff' }} />
+                  <WrapperTextHeaderSmall>Hỗ trợ</WrapperTextHeaderSmall>
+                </div>
+              ) : user?.access_token ? (
+                <div onClick={() => navigate('/chatpage')} style={{ cursor: 'pointer' }}>
+                  <WechatOutlined style={{ fontSize: '30px', color: '#fff' }} />
+                  <WrapperTextHeaderSmall>Hỗ trợ</WrapperTextHeaderSmall>
+                </div>
+              ) : (
+                <div onClick={() => navigate('/sign-in')} style={{ cursor: 'pointer' }}>
+                  <WechatOutlined style={{ fontSize: '30px', color: '#fff' }} />
+                  <WrapperTextHeaderSmall>Hỗ trợ</WrapperTextHeaderSmall>
+                </div>
+              )
+            }
 
           </span>
           <span>
